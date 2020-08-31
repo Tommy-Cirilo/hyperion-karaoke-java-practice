8c54fb68aee6ae81d54e6f0d2bdee434008f8144
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-31 02:41:17 -0500

    working on number 6

---
+++ src/LyricsPlayer.java
@@ -0,0 +1,21 @@
+public class LyricsPlayer extends Player{
+    //creates static color constants
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+
+    private String textColor = "default";
+    @Override
+    public void play(Album album) {
+
+    }
+
+    @Override
+    public void play(Song song) {
+
+    }
+}
