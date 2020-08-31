567fb8af299649972950e3fc7ccbe260d1ce3559
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-31 02:16:10 -0500

    finished number 4

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,5 @@
-public abstract Player {
+//add an abstract Player class that implements the Playable interface
+public abstract class Player implements Playable{
+    final int WORD_CADENCE = 600;
+    final int INTRO_PAUSE = 7000;
 }
